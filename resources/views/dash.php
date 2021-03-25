b3f6e0e59fc4cf9f48cc5004f8cce9bbf473fc01
Shiv Om <shivombhardwaj3156@gmail.com>
2021-03-25 23:19:19 +0530

    dashboard

---
+++ resources/views/dash.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+  <head>
+    
+  <meta charset="utf-8">
+  
+  <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="check.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+  </head>
+  <body>
+    <nav>
+      <input type="checkbox" id="check">
+      <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+      </label>
+      <label class="logo">©FREELANCER</label>
+      <ul>
+   
+        <li><a href="#">Jobs</a></li>
+        <li><a href="#">Talent</a></li>
+        <li><a href="#">Report</a></li>
+        <li><a href="#">Message</a></li>
+      </ul>
+    </nav>
+   
+
+
+    <div class="regform">
+        <h1>
+ POST JOB</h1>
+</div>
+<div class="main">
+    
+        <form method="POST">
+            <div id="name">
+                <h2 class="name">
+Project Name </h2>
+<input class="firstname" type="text" name="pname"><br>
+               
+            </div>
+<h2 class="name">
+
+    
+    
+            <h2 class="name" style="height:-1px">
+Project Category</h2>
+<h2 id="new">
+<select class="option" name="subject">
+                <option disabled="disabled" selected="selected">--Choose option--</option>
+                <option>Graphic Design</option>
+                <option>Full Stack Development</option>
+                <option>Language Localization</option>
+                <option>Transcription</option>
+                <option>Writing Tutoring</option>
+            </select>
+
+   
+
+ 
+            <h2 id="coustomer">
+Description</h2>
+
+   <h4 id="hello">
+   <textarea id="w3review" name="w3review" rows="4" cols="50" style="height:50px,width:px" >
+
+  </textarea>
+
+<h2 id="coustomer">
+What type of project do you have?</h2>
+<label class="radio">
+                <input class="radio-one" type="radio" checked="checked" name="rdiobtn">
+                <span class="checkmark"></span>
+                one time project
+            </label>
+    
+            <label class="radio">
+                <input class="radio-two" type="radio" name="rdiobtn">
+                <span class="checkmark"></span>
+               on going project
+            </label>
+
+            <label class="radio">
+                <input class="radio-two" type="radio" name="rdiobtn">
+                <span class="checkmark"></span>
+               complex project
+            </label>
+           
+           
+
+ <h3><h3 id="coustomer">
+
+   What would you like to do</h3>
+<label class="radio">
+                <input class="radio-one" type="radio" checked="checked" name="rdiobtn">
+                <span class="checkmark"></span>
+                short time  project
+            </label>
+    
+            <label class="radio"> 
+                <input class="radio-two" type="radio" name="rdiobtn">
+                <span class="checkmark"></span>
+               longer term project
+            </label>
+
+            <h4><h4 id="coustomer">
+
+            What level of experience should your freelancer have?</h4>
+               <label class="radio">
+                <input class="radio-one" type="radio" checked="checked" name="rdiobtn">
+                <span class="checkmark"></span>
+                Entry Level
+            </label>
+    
+            <label class="radio"> 
+                <input class="radio-two" type="radio" name="rdiobtn">
+                <span class="checkmark"></span>
+               Intermediate
+            </label>
+            <label class="radio"> 
+                <input class="radio-two" type="radio" name="rdiobtn">
+                <span class="checkmark"></span>
+              Expert
+            </label>
+
+
+<h5><h5 id="coustomer">
+
+How many people do you need for this job?</h5>
+               <label class="radio">
+                <input class="radio-one" type="radio" checked="checked" name="rdiobtn">
+                <span class="checkmark"></span>
+                Only One
+            </label>
+    
+            <label class="radio"> 
+                <input class="radio-two" type="radio" name="rdiobtn">
+                <span class="checkmark"></span>
+            More than one
+            </label>
+
+
+            <h6><h6 id="coustomer">
+
+            How long do you expect this project to last?</h6>
+               <label class="radio">
+                <input class="radio-one" type="radio" checked="checked" name="rdiobtn">
+                <span class="checkmark"></span>
+                More than 6 month
+            </label>
+    
+            <label class="radio"> 
+                <input class="radio-two" type="radio" name="rdiobtn">
+                <span class="checkmark"></span>
+           3 to 6 month
+            </label>
+            <label class="radio"> 
+                <input class="radio-two" type="radio" name="rdiobtn">
+                <span class="checkmark"></span>
+           1 to 3 month
+            </label><label class="radio"> 
+                <input class="radio-two" type="radio" name="rdiobtn">
+                <span class="checkmark"></span>
+        less than 1 month
+            </label>
+            
+            
+            
+            <h5><h5 id="coustomer">
+
+Do you have a time requirement for this project?</h5>
+               <label class="radio">
+                <input class="radio-one" type="radio" checked="checked" name="rdiobtn">
+                <span class="checkmark"></span>
+               More than 30/week
+            </label>
+    
+            <label class="radio"> 
+                <input class="radio-two" type="radio" name="rdiobtn">
+                <span class="checkmark"></span>
+           less than 20 per week
+            </label>
+
+            <label class="radio"> 
+                <input class="radio-two" type="radio" name="rdiobtn">
+                <span class="checkmark"></span>
+           I don't know yet
+            </label>
+
+            <button type="submit">POST JOB </button>
+    
+     
+
+
+
+
+            
+        </form>
+</div>
+
+
+  
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
