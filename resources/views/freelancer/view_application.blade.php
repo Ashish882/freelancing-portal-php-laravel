dc64dd0ca6fa6aa456c73a9fcb4d838611924265
Shiv Om <shivombhardwaj3156@gmail.com>
2021-04-07 23:34:03 +0530

    u

--- resources/views/freelancer/view_application.blade.php
+++ resources/views/freelancer/view_application.blade.php
@@ -31,32 +31,17 @@ Content body start
 </div>
 
 -->
-<div class="dropdown custom-dropdown mb-0 mr-4 mt-3 mt-sm-0 mb-2">
-<div class="btn border border-primary text-black btn-rounded" role="button" data-toggle="dropdown" aria-expanded="false">
-    <svg class="mr-2 scale5" width="14" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M10.2932 16.293L8.00016 18.5859V3C8.00016 2.73478 7.89481 2.48043 7.70727 2.29289C7.51973 2.10536 7.26538 2 7.00016 2C6.73495 2 6.48059 2.10536 6.29306 2.29289C6.10552 2.48043 6.00016 2.73478 6.00016 3V18.5859L3.70716 16.293C3.51856 16.1108 3.26596 16.01 3.00376 16.0123C2.74156 16.0146 2.49075 16.1198 2.30534 16.3052C2.11994 16.4906 2.01477 16.7414 2.01249 17.0036C2.01021 17.2658 2.111 17.5184 2.29316 17.707L6.29316 21.707C6.48086 21.8942 6.73513 21.9993 7.00021 21.9993C7.2653 21.9993 7.51956 21.8942 7.70726 21.707L11.7073 17.707C11.8901 17.5185 11.9914 17.2657 11.9894 17.0031C11.9874 16.7405 11.8822 16.4893 11.6965 16.3036C11.5109 16.118 11.2596 16.0128 10.997 16.0108C10.7345 16.0088 10.4816 16.1102 10.2932 16.293Z" fill="#40189D"></path>
-        <path d="M11.0002 6H21.0002C21.2655 6 21.5198 5.89464 21.7074 5.7071C21.8949 5.51957 22.0002 5.26521 22.0002 5C22.0002 4.73478 21.8949 4.48043 21.7074 4.29289C21.5198 4.10536 21.2655 4 21.0002 4H11.0002C10.735 4 10.4807 4.10536 10.2931 4.29289C10.1056 4.48043 10.0002 4.73478 10.0002 5C10.0002 5.26521 10.1056 5.51957 10.2931 5.7071C10.4807 5.89464 10.735 6 11.0002 6Z" fill="#40189D"></path>
-        <path d="M21.0002 8H11.0002C10.735 8 10.4807 8.10536 10.2931 8.29289C10.1056 8.48043 10.0002 8.73478 10.0002 9C10.0002 9.26521 10.1056 9.51957 10.2931 9.7071C10.4807 9.89464 10.735 10 11.0002 10H21.0002C21.2655 10 21.5198 9.89464 21.7074 9.7071C21.8949 9.51957 22.0002 9.26521 22.0002 9C22.0002 8.73478 21.8949 8.48043 21.7074 8.29289C21.5198 8.10536 21.2655 8 21.0002 8Z" fill="#40189D"></path>
-        <path d="M18.0002 12H11.0002C10.735 12 10.4807 12.1054 10.2931 12.2929C10.1056 12.4804 10.0002 12.7348 10.0002 13C10.0002 13.2652 10.1056 13.5196 10.2931 13.7071C10.4807 13.8947 10.735 14 11.0002 14H18.0002C18.2655 14 18.5198 13.8947 18.7074 13.7071C18.8949 13.5196 19.0002 13.2652 19.0002 13C19.0002 12.7348 18.8949 12.4804 18.7074 12.2929C18.5198 12.1054 18.2655 12 18.0002 12Z" fill="#40189D"></path>
-    </svg>
-    Newest
-    <i class="las la-angle-down scale5 text-primary ml-3"></i>
-</div>
-<div class="dropdown-menu dropdown-menu-right">
-    <a class="dropdown-item" href="javascript:void(0);">Details</a>
-    <a class="dropdown-item text-danger" href="javascript:void(0);">Cancel</a>
-</div>
-</div>
+
 <a href="javascript:void(0);" class="mr-3 mb-2">
 <span class="border border-primary rounded-circle d-block sharp-lg">
-    <svg class="scale5" width="14" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg class="scale5" width="14" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"  style="margin-top:15px;">
         <path d="M6 7.5H3C2.17275 7.5 1.5 6.82687 1.5 6V3C1.5 2.17313 2.17275 1.5 3 1.5H6C6.82725 1.5 7.5 2.17313 7.5 3V6C7.5 6.82687 6.82725 7.5 6 7.5ZM3 3V6H6.00113L6 3H3ZM22.5 4.5C22.5 4.08544 22.1642 3.75 21.75 3.75H9.75C9.33581 3.75 9 4.08544 9 4.5C9 4.91456 9.33581 5.25 9.75 5.25H21.75C22.1642 5.25 22.5 4.91456 22.5 4.5ZM6 15H3C2.17275 15 1.5 14.3269 1.5 13.5V10.5C1.5 9.67313 2.17275 9 3 9H6C6.82725 9 7.5 9.67313 7.5 10.5V13.5C7.5 14.3269 6.82725 15 6 15ZM3 10.5V13.5H6.00113L6 10.5H3ZM22.5 12C22.5 11.5854 22.1642 11.25 21.75 11.25H9.75C9.33581 11.25 9 11.5854 9 12C9 12.4146 9.33581 12.75 9.75 12.75H21.75C22.1642 12.75 22.5 12.4146 22.5 12ZM6 22.5H3C2.17275 22.5 1.5 21.8269 1.5 21V18C1.5 17.1731 2.17275 16.5 3 16.5H6C6.82725 16.5 7.5 17.1731 7.5 18V21C7.5 21.8269 6.82725 22.5 6 22.5ZM3 18V21H6.00113L6 18H3ZM22.5 19.5C22.5 19.0854 22.1642 18.75 21.75 18.75H9.75C9.33581 18.75 9 19.0854 9 19.5C9 19.9146 9.33581 20.25 9.75 20.25H21.75C22.1642 20.25 22.5 19.9146 22.5 19.5Z" fill="#40189D"></path>
     </svg>
 </span>
 </a>
 <a href="javascript:void(0);" class="mb-2">
 <span class="bg-primary rounded-circle d-block sharp-lg">
-    <svg class="scale5" width="14" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg class="scale5" width="14" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"   style="margin-top:15px;">
         <path d="M10 1H2C1.73478 1 1.48043 1.10536 1.29289 1.29289C1.10536 1.48043 1 1.73478 1 2V10C1 10.2652 1.10536 10.5196 1.29289 10.7071C1.48043 10.8946 1.73478 11 2 11H10C10.2652 11 10.5196 10.8946 10.7071 10.7071C10.8946 10.5196 11 10.2652 11 10V2C11 1.73478 10.8946 1.48043 10.7071 1.29289C10.5196 1.10536 10.2652 1 10 1ZM9 9H3V3H9V9Z" fill="white"></path>
         <path d="M22 1H14C13.7348 1 13.4804 1.10536 13.2929 1.29289C13.1054 1.48043 13 1.73478 13 2V10C13 10.2652 13.1054 10.5196 13.2929 10.7071C13.4804 10.8946 13.7348 11 14 11H22C22.2652 11 22.5196 10.8946 22.7071 10.7071C22.8946 10.5196 23 10.2652 23 10V2C23 1.73478 22.8946 1.48043 22.7071 1.29289C22.5196 1.10536 22.2652 1 22 1ZM21 9H15V3H21V9Z" fill="white"></path>
         <path d="M10 13H2C1.73478 13 1.48043 13.1054 1.29289 13.2929C1.10536 13.4804 1 13.7348 1 14V22C1 22.2652 1.10536 22.5196 1.29289 22.7071C1.48043 22.8946 1.73478 23 2 23H10C10.2652 23 10.5196 22.8946 10.7071 22.7071C10.8946 22.5196 11 22.2652 11 22V14C11 13.7348 10.8946 13.4804 10.7071 13.2929C10.5196 13.1054 10.2652 13 10 13ZM9 21H3V15H9V21Z" fill="white"></path>
@@ -76,9 +61,9 @@ Content body start
                 </th>
                 <th>ID</th>
                 <th>Date Applied</th>
-                <th>Company</th>
+                <th>Company </th>
                 <th>Type</th>
-                <th>Postition</th>
+              
                 <th>Contact</th>
                 <th>Status</th>
             </tr>
@@ -105,11 +90,10 @@ Content body start
     </div>
 </td>
 <td>FULLTIME</td>
-<td>Senior UX Designer</td>
+
 <td>
     <div class="d-flex">
-        <a class="contact-icon mr-3" href="#"><i class="fa fa-phone" aria-hidden="true"></i></a>
-        <a class="contact-icon" href="#"><i class="las la-envelope"></i></a>
+        <a class="contact-icon   href="#"><i class="las la-envelope" style="margin-top:10px;" ></i></a>
     </div>
 </td>
 <td>
@@ -152,10 +136,10 @@ Content body start
     </div>
 </td>
 <td>PART TIME</td>
-<td>Junior UI Designer</td>
+
 <td>
     <div class="d-flex">
-        <a class="contact-icon" href="#"><i class="las la-envelope"></i></a>
+        <a class="contact-icon"  href="#"><i class="las la-envelope" style="margin-top:10px;" ></i></a>
     </div>
 </td>
 <td>
@@ -198,11 +182,11 @@ Content body start
     </div>
 </td>
 <td>FREELANCE</td>
-<td>Intern UI Designer</td>
+
 <td>
     <div class="d-flex">
-        <a class="contact-icon mr-3" href="#"><i class="fa fa-phone" aria-hidden="true"></i></a>
-        <a class="contact-icon" href="#"><i class="las la-envelope"></i></a>
+        <a class="contact-icon mr-3" href="#"><i class="fa fa-phone" style="margin-top:10px;"  aria-hidden="true"></i></a>
+        <a class="contact-icon"   href="#"><i class="las la-envelope" style="margin-top:10px;" ></i></a>
     </div>
 </td>
 <td>
@@ -245,10 +229,10 @@ Content body start
     </div>
 </td>
                 <td>FREELANCE</td>
-                <td>Intern UI Designer</td>
+             
 <td>
 <div class="d-flex">
-    <a class="contact-icon" href="#"><i class="fa fa-phone" aria-hidden="true"></i></a>
+    <a class="contact-icon" href="#"><i class="fa fa-phone" style="margin-top:10px;"  aria-hidden="true"></i></a>
 </div>
 </td>
 <td>
@@ -291,11 +275,11 @@ Content body start
 </div>
 </td>
 <td>FULLTIME</td>
-<td>Senior UX Designer</td>
+
 <td>
 <div class="d-flex">
-    <a class="contact-icon mr-3" href="#"><i class="fa fa-phone" aria-hidden="true"></i></a>
-    <a class="contact-icon" href="#"><i class="las la-envelope"></i></a>
+    <a class="contact-icon mr-3" href="#"><i class="fa fa-phone"  style="margin-top:10px;"  aria-hidden="true"></i></a>
+    <a class="contact-icon" href="#"><i class="las la-envelope" style="margin-top:10px;" ></i></a>
 </div>
 </td>
 <td>
@@ -338,10 +322,10 @@ Content body start
                     </div>
                 </td>
                 <td>PART TIME</td>
-                <td>Junior UI Designer</td>
+           
                 <td>
                     <div class="d-flex">
-                        <a class="contact-icon" href="#"><i class="fa fa-phone" aria-hidden="true"></i></a>
+                        <a class="contact-icon"   href="#"><i class="fa fa-phone" style="margin-top:10px;"  aria-hidden="true"></i></a>
                     </div>
                 </td>
                 <td>
