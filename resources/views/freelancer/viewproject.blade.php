048903053245ebce588a1d3b483e5a7d4b17ed09
Ashish Sharma <hzashish882@gmail.com>
2021-04-18 00:13:24 +0530

    commit

--- resources/views/freelancer/viewproject.blade.php
+++ resources/views/freelancer/viewproject.blade.php
@@ -12,8 +12,12 @@
 <div class="col-md-6">
 
 <div class="form-group" style="font-size:25px";>
-   <p> Reputation management
+   <p><h2 style="font-size: 30px;
+    line-height: var(--line-height-h2);
+    font-weight: var(--heading-weight);
+    letter-spacing: var(--heading-letter-spacing-h2);">Reputation management</h2>
    </p>
+   <hr>
   </div>
 
   <div class="form-group" ;>
@@ -25,22 +29,49 @@
    <p><i class="fa fa-map-marker" aria-hidden="true"></i> Worldwide</p>
    
  
-   <div class="form-group" ;>
+   <div class="form-group">
    <p>I have restaurant in maharashtra which has 3 stars review on google , and I want to bring it at 5 stars.  I want my freelancer to be up to the mark with each and every work I sign to them .
    </p>
    
   </div>
   
 
-  <div class="form-group" ;>
+  <div class="form-group">
    <p>I have restaurant in maharashtra which has 3 stars review on google , and I want to bring it at 5 stars.  I want my freelancer to be up to the mark with each and every work I sign to them .
    </p>
-   
-   
-   
-
   </div>
 
+  <div class="col-12">
+
+  <div class="row">
+  <div class="col-4">
+  Less than 30 hrs/week
+Hourly
+  </div>
+
+  <div class="col-4">
+
+  Less than a month
+  
+  </div>
+
+  <div class="col-4">
+
+  Intermediate
+  
+  </div>
+  
+  
+  
+  </div>
+  
+  
+  </div>
+<br>
+<br>
+<br>
+
+<a href=""  class="cfe-ui-job-skill up-skill-badge m-0-left m-0-top m-xs-bottom">Frontend Development</a>
  
 
 
