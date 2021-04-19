4a1b9986b0683208b55b44a6309110e74d7c33ad
Shiv Om <shivombhardwaj3156@gmail.com>
2021-04-19 16:13:36 +0530

    updagte

--- resources/views/freelancer/viewproject.blade.php
+++ resources/views/freelancer/viewproject.blade.php
@@ -16,30 +16,29 @@
     line-height: var(--line-height-h2);
     font-weight: var(--heading-weight);
     letter-spacing: var(--heading-letter-spacing-h2);">Reputation management</h2>
-   </p>
-   <hr>
+   </p><hr style="border: 1px solid black;">
+   
+  
+
   </div>
 
   <div class="form-group" ;>
    <p> Search Engine Optimization
    </p>
    
-   <p><i class="fas fa-cloud"></i> Specialized profiles can help you better highlight your expertise when submitting proposals to jobs like these.
+   <p><i class="fa fa-bullhorn" aria-hidden="true"></i> &nbsp;&nbsp;Specialized profiles can help you better highlight your expertise when submitting proposals to jobs like these.
    </p>
-   <p><i class="fa fa-map-marker" aria-hidden="true"></i> Worldwide</p>
+   <p><i class="fa fa-map-marker" aria-hidden="true"></i> &nbsp;&nbsp;Worldwide</p>
    
- 
+   <hr style="border: 1px solid black;">
    <div class="form-group">
    <p>I have restaurant in maharashtra which has 3 stars review on google , and I want to bring it at 5 stars.  I want my freelancer to be up to the mark with each and every work I sign to them .
    </p>
    
   </div>
-  
+  <hr style="border: 1px solid black;">
 
-  <div class="form-group">
-   <p>I have restaurant in maharashtra which has 3 stars review on google , and I want to bring it at 5 stars.  I want my freelancer to be up to the mark with each and every work I sign to them .
-   </p>
-  </div>
+  
 
   <div class="col-12">
   
@@ -68,20 +67,29 @@ Hourly
   
   
   </div>
-<br>
-<br>
+  <hr style="border: 1px solid black;">
 <br>
 
-<a href=""  class="cfe-ui-job-skill up-skill-badge m-0-left m-0-top m-xs-bottom">Frontend Development</a>
+
+<h3>Skills and Expertise</h3>
+<a href=""  class="cfe-ui-job-skill up-skill-badge m-0-left m-0-top m-xs-bottom">Frontend Development</a>&nbsp;&nbsp;
+<a href=""  class="cfe-ui-job-skill up-skill-badge m-0-left m-0-top m-xs-bottom">SEO expert</a>&nbsp;&nbsp;
+<a href=""  class="cfe-ui-job-skill up-skill-badge m-0-left m-0-top m-xs-bottom">Backend Development</a>&nbsp;&nbsp;
+<a href=""  class="cfe-ui-job-skill up-skill-badge m-0-left m-0-top m-xs-bottom">Full stack Developer</a>&nbsp;&nbsp;
+<hr style="border: 1px solid black;">
  
+<div class="form-group" style="font-size:25px,padding-top:50px";>
+<br>
+<button type="button" class="btn btn-success">Submit Proposal</button>
+   
+  </div>
 
 
 
   </label>
-
-
+  
         </form>
-
+       
         </div><!-- end of row-->   
   </div><!-- end of container-->   
 </section>
