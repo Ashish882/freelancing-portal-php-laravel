964f0ef049223bbd650b74ac986ffb3b79cb53a7
aroraG <shivamarora050620@gmail.com>
2021-04-25 00:25:53 +0530

    view projrct

--- resources/views/freelancer/viewproject.blade.php
+++ resources/views/freelancer/viewproject.blade.php
@@ -1,15 +1,15 @@
 @extends('freelancer.header')
 
-<!--**********************************
+<!--************
 	Content body start
-***********************************-->
+*************-->
 <div class="content-body">
 	<!-- row -->
 	<div class="container-fluid">		
 	<section class="das">
 <div class="container"  style="background: #fff; padding:50px;">
 <div class="row">
-<div class="col-md-6">
+<div class="col-md-8">
 
 <div class="form-group" style="font-size:25px";>
    <p><h2 style="font-size: 30px;
@@ -80,7 +80,7 @@ Hourly
  
 <div class="form-group" style="font-size:25px,padding-top:50px";>
 <br>
-<button type="button" class="btn btn-success">Submit Proposal</button>
+
    
   </div>
 
@@ -90,15 +90,47 @@ Hourly
   
         </form>
        
+
+
+
+</div>
+
+
+
         </div><!-- end of row-->   
-  </div><!-- end of container-->   
+        
+        <div class="col-md-4">
+        <div>
+        <button type="button" class="btn btn-success" style="width:200px;">Submit Proposal</button>
+        </div>
+        <hr style="border: 1px solid black;width:300px;">
+        <h7>
+        About the client</h7>
+ <br> <i class="fab fa-btc"style="font-size:15px;"></i> <p>Payment is verified</p>
+
+ <p><b>United States</b></p>
+ <p>Atlanta 02:35 pm
+</p>
+
+<p><b>445 jobs posted</b></p>
+ <p>8% hire rate, 20 open jobs
+</p>
+<p><b>37.09 /hr avg hourly rate paid</b></p>
+ <p>4 hours
+</p>
+
+<p>Member since Jul 19, 2017</p>
+<hr style="border: 1px solid black;">
+  </div><!-- end of container-->  
+  
 </section>
 	</div>
 </div>
-<!--**********************************
+<!--************
 	Content body end
-***********************************-->
+*************-->
 
 @extends('freelancer.footer')
 
 
+<link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.7/css/all.css">
