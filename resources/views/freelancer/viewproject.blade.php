7bb7d6869e8cbabeb766febd52a0d57be15858c9
Shiv Om <shivombhardwaj3156@gmail.com>
2021-04-25 12:35:39 +0530

    update\

--- resources/views/freelancer/viewproject.blade.php
+++ resources/views/freelancer/viewproject.blade.php
@@ -8,7 +8,7 @@
 	<div class="container-fluid">		
 	<section class="das">
 <div class="container"  style="background: #fff; padding:50px;">
-<div class="row">
+<div class="row"  style="border:1px solid black;border-radius: 25px;text-align:center;width:100%;font-size:100%;padding-top:0px;padding-left:50px;padding-right:50px;">
 <div class="col-md-8">
 
 <div class="form-group" style="font-size:25px";>
@@ -16,13 +16,13 @@
     line-height: var(--line-height-h2);
     font-weight: var(--heading-weight);
     letter-spacing: var(--heading-letter-spacing-h2);">Reputation management</h2>
-   </p><hr style="border: 1px solid black;">
+   </p>
    
   
 
   </div>
 
-  <div class="form-group" ;>
+  <div class="form-group">
    <p> Search Engine Optimization
    </p>
    
@@ -30,13 +30,13 @@
    </p>
    <p><i class="fa fa-map-marker" aria-hidden="true"></i> &nbsp;&nbsp;Worldwide</p>
    
-   <hr style="border: 1px solid black;">
-   <div class="form-group">
+  <hr>
+   <div class="form-group" >
    <p>I have restaurant in maharashtra which has 3 stars review on google , and I want to bring it at 5 stars.  I want my freelancer to be up to the mark with each and every work I sign to them .
    </p>
    
   </div>
-  <hr style="border: 1px solid black;">
+ 
 
   
 
@@ -67,17 +67,16 @@ Hourly
   
   
   </div>
-  <hr style="border: 1px solid black;">
+
 <br>
 
-
+<hr>
 <h3>Skills and Expertise</h3>
 <a href=""  class="cfe-ui-job-skill up-skill-badge m-0-left m-0-top m-xs-bottom">Frontend Development</a>&nbsp;&nbsp;
 <a href=""  class="cfe-ui-job-skill up-skill-badge m-0-left m-0-top m-xs-bottom">SEO expert</a>&nbsp;&nbsp;
 <a href=""  class="cfe-ui-job-skill up-skill-badge m-0-left m-0-top m-xs-bottom">Backend Development</a>&nbsp;&nbsp;
 <a href=""  class="cfe-ui-job-skill up-skill-badge m-0-left m-0-top m-xs-bottom">Full stack Developer</a>&nbsp;&nbsp;
-<hr style="border: 1px solid black;">
- 
+
 <div class="form-group" style="font-size:25px,padding-top:50px";>
 <br>
 
@@ -99,25 +98,32 @@ Hourly
 
         </div><!-- end of row-->   
         
-        <div class="col-md-4" style="border:5px solid;border-radius: 25px;text-align:center;width:5%;font-size:100%;padding-top:50px;">
+        <div class="col-md-4" style=" 
+        border-left: 1px solid black;
+  height: 600px;
+  
+  right: 10px;
+  padding-top:10px;
+ 
+  " >
 
        
      <b>
         <h7>
         About the client</h7></b>
  <br> <i class="fab fa-btc"style="font-size:15px;"></i> <p>Payment is verified</p>
-
+<hr style="padding-left:;">
  <p><b>United States</b></p>
  <p>Atlanta 02:35 pm
 </p>
-
+<hr>
 <p><b>445 jobs posted</b></p>
  <p>8% hire rate, 20 open jobs
 </p>
 <p><b>37.09 /hr avg hourly rate paid</b></p>
  <p>4 hours
 </p>
-
+<hr>
 <p>Member since Jul 19, 2017</p>
 <br>
 <br>
