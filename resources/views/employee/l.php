29ab36a61abae68023b72e4bce6977c622243fa7
Shiv Om <shivombhardwaj3156@gmail.com>
2021-05-02 19:13:59 +0530

    vvfv

--- resources/views/employee/l.php
+++ resources/views/employee/l.php
@@ -63,115 +63,135 @@
 					<div class="d-flex col-lg-12 col-sm-6 align-items-center">
 						<div>
 
-            <section>
-<div class="card" >
-  
+<section class="das" >
+<div class="container"  style="background: #fff; padding:50px;">
+<div class="row" > 
 
-    <div class="form-group">
- <label style="padding-bottom:10px;font-size:20px;font-weight: bold;padding-top:20px;" for="pname">Choose a name for your project</label>
- <input type="text" class="form-control" id="pname" placeholder="Enter name of your job project" style="width:100%;height:50px;font-size:15px;">
+<form  method="POST" action="addproject">
+    @csrf
+
+<div class="col-md-12">
+<div class="form-group">
+&nbsp; <label style="padding-bottom:10px;width:400px;" for="pname">Title</label>
+&nbsp;  <input type="text" class="form-control" id="pname" placeholder="Enter name of your job project" style="width:100%">
   </div>
 
-  <p  style="padding-bottom:10px;font-size:20px;font-weight: bold;padding-top:20px;">  What type of project do you have?
-  </p>
-  <div class="form-check">
  
-   <input class="form-check-input" type="radio" name="exampleRadios1" id="exampleRadios1" value="option1" checked>
-  <label class="form-check-label " style="  font-size: 15px;color: black;" for="exampleRadios1">
+
+</div>
+</div>
+
+<div class="row">
+
+<div class="col-md-12">
+
+<div class="form-group">
+  <div class="form-check">
+  <p>What type of project do you have?
+  </p>
+  &nbsp;  <input class="form-check-input" type="radio" name="exampleRadios1" id="exampleRadios1" value="option1" checked>
+  <label class="form-check-label" for="exampleRadios1">
     One Time Project
   </label>
 </div>
 <div class="form-check">
- <input class="form-check-input" type="radio" name="exampleRadios1" id="exampleRadios2" value="option2">
-  <label class="form-check-label" style="  font-size: 15px;color: black;" for="exampleRadios2">
+&nbsp;   <input class="form-check-input" type="radio" name="exampleRadios1" id="exampleRadios2" value="option2">
+  <label class="form-check-label" for="exampleRadios2">
     Ongoing Project
   </label>
 </div>
 <div class="form-check">
- <input class="form-check-input" type="radio" name="exampleRadios1" id="exampleRadios3" value="option3">
-  <label class="form-check-label" style="  font-size: 15px;color:black;" for="exampleRadios3">
+&nbsp;   <input class="form-check-input" type="radio" name="exampleRadios1" id="exampleRadios3" value="option3">
+  <label class="form-check-label" for="exampleRadios3">
     Complex Project
   </label>
+</div>
+</div>
+
+
+</div>
 </div>
 
 
 
-<p style="padding-bottom:10px;font-size:20px;font-weight: bold;padding-top:20px;">What level of experience should your freelancer have?
+
+
+  <div class="col-md-12" style="padding-top:50px;">
+<div class="form-check"><p>What level of experience should your freelancer have?
 </p>
-<div class="form-check">
   <input class="form-check-input" type="radio" name="exampleRadios2" id="exampleRadios1" value="option1" >
-  <label class="form-check-label" style="  font-size: 15px;color: black;" for="exampleRadios1">
+  <label class="form-check-label" for="exampleRadios1">
     Entry Level
   </label>
 </div>
 <div class="form-check">
   <input class="form-check-input" type="radio" name="exampleRadios2" id="exampleRadios2" value="option2">
-  <label class="form-check-label" style="  font-size: 15px;color: black;" for="exampleRadios2">
+  <label class="form-check-label" for="exampleRadios2">
    Intermediate
   </label>
 </div>
 <div class="form-check">
   <input class="form-check-input" type="radio" name="exampleRadios2" id="exampleRadios3" value="option3" >
-  <label class="form-check-label" style="  font-size: 15px;color: black;" for="exampleRadios3">
+  <label class="form-check-label" for="exampleRadios3">
  Expert
   </label>
 </div>
+</div>
+
+<div class="col-md-12" style="padding-top:50px;">
 
 
-
-
-
-<div class="est" style="padding-bottom:10px;font-size:20px;font-weight: bold;padding-top:20px;" >
+  <div class="est" style="margin-top:50px;" >
   <p>
         Enter your estimated budget
 </p>
 <div class="input-group mb-3">
   <div class="input-group-prepend">
-    <span class="input-group-text">Rs</span>
+    <span class="input-group-text">$</span>
   
   </div>
-  <input type="text" name="price" class="form-control" aria-label="Amount (to the nearest dollar)">
+  <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
 </div>
 
 </div>
 
+</div>
 
-
-
-
-
-<div class="form-group"><p style="padding-bottom:10px;font-size:20px;font-weight: bold; ">Additional project files (optional)</p>
+<div class="col-md-12">
+<div class="form-group"><p>Additional project files (optional)</p>
     <label for="exampleFormControlFile1"></label>
     <input type="file" class="form-control-file" id="exampleFormControlFile1">
-    <p style="  font-size: 15px;color: black;">You may attach up to 5 files under 100 MB each
+    <p>You may attach up to 5 files under 100 MB each
     </p>
   </div>
 
 
-  <div class="form-group">
-
-<label style="padding-bottom:10px;font-size:20px;font-weight: bold;padding-top:20px;" for="exampleFormControlTextarea1">Tell us more about your project</label><br>
-
-<textarea name="dis" class="form-control" style="resize:none;height:120px;" id="exampleFormControlTextarea1" rows="3" ></textarea>
+</div>
 
 
-
+<div class="col-md-12">
 
 <div class="form-group">
- <label style="padding-bottom:10px;font-size:20px;font-weight: bold;padding-top:20px;" for="pcat">Skills Required</label>
-<input type="text" class="form-control" name="pcat" placeholder="Enter the skills" style="width:100%;height:50px;font-size:15px;">
+
+<label style="padding-bottom:10px;padding-top:50px;" for="exampleFormControlTextarea1">Description</label><br>
+
+<textarea class="form-control" id="exampleFormControlTextarea1" rows="3" ></textarea>
 </div>
 
 
+</div>
 
-<div class="but" style="padding-top:20px;width:250px;">
-<input class="btn btn-primary" type="submit" value="Submit" style="padding-right: :500px">
+<div class="but">
+           <input class="btn btn-primary" type="submit" value="Submit" style="padding-right: :500px">
 </div>
 
 
+        </form>
 
-    </div>
-    </section>
+        </div><!-- end of row-->   
+  </div><!-- end of container-->   
+</section>
+				
 						</div>
 					</div>
 		</div>
